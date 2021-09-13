098fd5e1c0fe52b6298b00baf31d994f9815b2b2
SQL <sodeeqsodeeq@gmail.com>
2021-09-13 09:36:13 -0700

    2nd

--- wintrust/settings.py
+++ wintrust/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*97^%3qiwk)+xpv_%l@uppn&s3ybabr(8v_v5!wbvvm2$do#br'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
